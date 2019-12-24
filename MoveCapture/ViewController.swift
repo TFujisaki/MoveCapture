1258959ded1333c5f9a725feca693e22819e70d4
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2019-12-24 09:05:56 +0900

    Initial Commit

---
+++ MoveCapture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveCapture
+//
+//  Created by 藤崎 達也 on 2019/12/24.
+//  Copyright © 2019年 藤崎 達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
