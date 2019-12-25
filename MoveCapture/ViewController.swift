4b87ef1a316371427bf62858d559db66997d0f4e
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2019-12-25 18:30:11 +0900

    実行エラーを修正

--- MoveCapture/ViewController.swift
+++ MoveCapture/ViewController.swift
@@ -24,7 +24,7 @@ import UIKit
 import AVFoundation
 import Photos
 
-class RecordViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
+class ViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
     let fileOutput = AVCaptureMovieFileOutput()
 
     var recordButton: UIButton!
@@ -35,15 +35,15 @@ class RecordViewController: UIViewController, AVCaptureFileOutputRecordingDelega
         setUpPreview()
     }
 
-func setUpPreview() {
+    func setUpPreview() {
         
-    let videoDevice = AVCaptureDevice.default(for: AVMediaType.video)
-    let audioDevice = AVCaptureDevice.default(for: AVMediaType.audio)
+        let videoDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        let audioDevice = AVCaptureDevice.default(for: AVMediaType.audio)
 
-    do {
-        if videoDevice == nil || audioDevice == nil {
-            throw NSError(domain: "device error", code: -1, userInfo: nil)
-        }
+        do {
+            if videoDevice == nil || audioDevice == nil {
+                throw NSError(domain: "device error", code: -1, userInfo: nil)
+            }
         let captureSession = AVCaptureSession()
 
         // video inputを capture sessionに追加
@@ -67,59 +67,61 @@ func setUpPreview() {
         captureSession.startRunning()
 
         setUpButton()
-    } catch {
+        } catch {
                 // エラー処理
+        }
     }
+
+    
+    func setUpButton() {
+        recordButton = UIButton(frame: CGRect(x: 0,y: 0,width: 120,height: 50))
+        recordButton.backgroundColor = UIColor.gray
+        recordButton.layer.masksToBounds = true
+        recordButton.setTitle("録画開始", for: UIControl.State.normal)
+        recordButton.layer.cornerRadius = 20.0
+        recordButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
+        recordButton.addTarget(self, action: #selector(ViewController.onClickRecordButton(sender:)), for: .touchUpInside)
+
+        self.view.addSubview(recordButton)
+    } 
+
+    @objc func onClickRecordButton(sender: UIButton) {
+        if !isRecording {
+            // 録画開始
+            let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+            let documentsDirectory = paths[0] as String
+            let filePath : String? = "\(documentsDirectory)/temp.mp4"
+            let fileURL : NSURL = NSURL(fileURLWithPath: filePath!)
+            fileOutput.startRecording(to: fileURL as URL, recordingDelegate: self)
+
+            isRecording = true
+            changeButtonColor(target: recordButton, color: UIColor.red)
+            recordButton.setTitle("録画中", for: .normal)
+        } else {
+            // 録画終了
+            fileOutput.stopRecording()
+
+            isRecording = false
+            changeButtonColor(target: recordButton, color: UIColor.gray)
+            recordButton.setTitle("録画開始", for: .normal)
+        }
+    }
+
+    func changeButtonColor(target: UIButton, color: UIColor) {
+        target.backgroundColor = color
+    }
+
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        // ライブラリへ保存
+        PHPhotoLibrary.shared().performChanges({
+            PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
+        }) { completed, error in
+            if completed {
+                print("Video is saved!")
+            }
+        }
+    }
+    
 }
-
-        func setUpButton() {
-            recordButton = UIButton(frame: CGRect(x: 0,y: 0,width: 120,height: 50))
-            recordButton.backgroundColor = UIColor.gray
-            recordButton.layer.masksToBounds = true
-            recordButton.setTitle("録画開始", for: UIControl.State.normal)
-            recordButton.layer.cornerRadius = 20.0
-            recordButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
-            recordButton.addTarget(self, action: #selector(RecordViewController.onClickRecordButton(sender:)), for: .touchUpInside)
-
-            self.view.addSubview(recordButton)
-        }
-
-        @objc func onClickRecordButton(sender: UIButton) {
-            if !isRecording {
-                // 録画開始
-                let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-                let documentsDirectory = paths[0] as String
-                let filePath : String? = "\(documentsDirectory)/temp.mp4"
-                let fileURL : NSURL = NSURL(fileURLWithPath: filePath!)
-                fileOutput.startRecording(to: fileURL as URL, recordingDelegate: self)
-
-                isRecording = true
-                changeButtonColor(target: recordButton, color: UIColor.red)
-                recordButton.setTitle("録画中", for: .normal)
-            } else {
-                // 録画終了
-                fileOutput.stopRecording()
-
-                isRecording = false
-                changeButtonColor(target: recordButton, color: UIColor.gray)
-                recordButton.setTitle("録画開始", for: .normal)
-            }
-        }
-
-        func changeButtonColor(target: UIButton, color: UIColor) {
-            target.backgroundColor = color
-        }
-
-        func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
-            // ライブラリへ保存
-            PHPhotoLibrary.shared().performChanges({
-                PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputFileURL)
-            }) { completed, error in
-                if completed {
-                    print("Video is saved!")
-                }
-            }
-        }
-    }
 
 
